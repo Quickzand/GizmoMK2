6c8b9ad19740b2445de0099addecda957ce52ba1
Matthew Sand <44583344+Quickzand@users.noreply.github.com>
2024-10-27 16:25:30 -0400

    Initial Commit

---
+++ GizmoMK2/GizmoMK2App.swift
@@ -0,0 +1,17 @@
+//
+//  GizmoMK2App.swift
+//  GizmoMK2
+//
+//  Created by Matthew Sand on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GizmoMK2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GizmoMK2Tests/GizmoMK2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GizmoMK2Tests.swift
+//  GizmoMK2Tests
+//
+//  Created by Matthew Sand on 10/27/24.
+//
+
+import Testing
+@testable import GizmoMK2
+
+struct GizmoMK2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
